ed4616fd17d5cead99bd48e52bb373f1728be69b
unknown <erananthu100@gmail.com>
2025-12-17 22:51:10 +0530

    Feat: Added bootstrap code to the appliation

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+#Dummy data
+data = [
+    {
+        'Title' : "FIRST BLOG",
+        'Author' : "Ananthu",
+        'Content' : "This is my first blog content",
+        'DatePosted' : "17-12-2025"
+    },
+    {
+        'Title' : "SECOND BLOG",
+        'Author' : "Jacky",
+        'Content' : "This is my Second blog content",
+        'DatePosted' : "18-12-2025"
+    }
+]
+
 def home(request):
-    return render(request,'blog/home.html')
+    context = {
+        'posts' : data
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request,'blog/about.html',{'title': 'About'})
